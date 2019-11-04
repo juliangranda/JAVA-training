886123a460e5d3ead10894718ea26e042efa4b28
juliangranda <juliangranda@hotmail.com>
2019-11-04 09:51:19 -0500

    reto-arrayList

---
+++ src/reto2/ArrayList_reto2.java
@@ -0,0 +1,81 @@
+package reto2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ArrayList_reto2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nombre;
+		int numero_carne;
+		int numero_creditos;
+		int valor_credito;
+		float descuento;
+		int tamaño;
+		int matriculaMasAlta = 0;
+		int carnetMatriculaMasAlta = 0;
+		
+		
+		//Estudiantes estudiante = new Estudiantes();
+		List<Estudiantes> miLista = new ArrayList();
+		
+		Scanner scaner = new Scanner(System.in);
+		System.out.println("ingrese numero de estudiantes: ");
+		tamaño = scaner.nextInt();
+		
+		Estudiantes estudiante = new Estudiantes();
+		for (int i = 0; i < tamaño; i++) {
+			
+			System.out.println("escriba nombre estudiante");
+			nombre = scaner.next();
+			estudiante.setNombre(nombre);
+			
+			System.out.println("escriba numero_carne estudiante");
+			numero_carne = scaner.nextInt();
+			estudiante.setNumero_carne(numero_carne);
+			
+			System.out.println("escriba numero_creditos estudiante");
+			numero_creditos = scaner.nextInt();
+			estudiante.setNumero_creditos(numero_creditos);
+			
+			System.out.println("escriba valor_credito estudiante");
+			valor_credito = scaner.nextInt();
+			estudiante.setValor_credito(valor_credito);
+			
+			System.out.println("escriba descuento estudiante");
+			descuento = scaner.nextFloat();
+			estudiante.setDescuento(descuento);
+			
+			Estudiantes datos = new Estudiantes(nombre, numero_carne, numero_creditos, valor_credito, descuento);
+			miLista.add(datos);
+			System.out.println(miLista.get(i));
+			
+		}
+
+		
+		System.out.println("-----------------");
+		
+        for(int x=0;x<miLista.size();x++) {
+			System.out.println( miLista.get(x).toString());
+      	}
+		
+		
+		for (int i = 0; i < miLista.size(); i++) {
+			
+			System.out.println("matricula estudiante: "+ miLista.get(i).getNombre() + " es del valor de : "+ miLista.get(i).matricula());
+			if(miLista.get(i).matricula() > matriculaMasAlta) {
+				matriculaMasAlta = miLista.get(i).matricula();
+				carnetMatriculaMasAlta = miLista.get(i).getNumero_carne();
+			}
+		}
+		System.out.println("la matricula mas alta es de : " + carnetMatriculaMasAlta);
+		
+
+		
+		
+
+	}
+
+}
---
+++ src/reto2/Estudiantes.java
@@ -0,0 +1,111 @@
+package reto2;
+
+public class Estudiantes {
+	
+	//atributos
+	private String nombre;
+	private int numero_carne;
+	private int numero_creditos;
+	private int valor_credito;
+	private float descuento;
+	
+	
+	public Estudiantes(){
+		
+		nombre ="";
+		numero_carne = 0;
+		numero_creditos = 0;
+		valor_credito = 0;
+		descuento = 0;
+	}
+	
+	public Estudiantes(String nombre, int numero_carne, int numero_creditos, int valor_credito, float descuento) {
+		this.nombre = nombre;
+		this.numero_carne = numero_carne;
+		this.numero_creditos = numero_creditos;
+		this.valor_credito = valor_credito;
+		this.descuento = descuento;
+	}
+
+	
+	public int matricula() {
+		int precio = 0;
+		float precio_total = 0;
+
+		precio = (this.numero_creditos * this.valor_credito);
+		precio_total = precio - precio*(this.descuento / 100);
+		return (int)precio_total;
+	}
+
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+
+	public int getNumero_carne() {
+		return numero_carne;
+	}
+
+
+
+	public void setNumero_carne(int numero_carne) {
+		this.numero_carne = numero_carne;
+	}
+
+
+
+	public int getNumero_creditos() {
+		return numero_creditos;
+	}
+
+
+
+	public void setNumero_creditos(int numero_creditos) {
+		this.numero_creditos = numero_creditos;
+	}
+
+
+
+	public int getValor_credito() {
+		return valor_credito;
+	}
+
+
+
+	public void setValor_credito(int valor_credito) {
+		this.valor_credito = valor_credito;
+	}
+
+
+
+	public float getDescuento() {
+		return descuento;
+	}
+
+
+
+	public void setDescuento(float descuento) {
+		this.descuento = descuento;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Estudiantes [nombre=" + nombre + ", numero_carne=" + numero_carne + ", numero_creditos="
+				+ numero_creditos + ", valor_credito=" + valor_credito + ", descuento=" + descuento + "]";
+	}
+	
+	
+
+	
+}
